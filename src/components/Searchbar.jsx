e682aed9d4ee018153fa3068cb213900d7e85f8d
seung-yuppy <songsy0107@gmail.com>
2025-03-20 13:45:15 +0900

    Feat.Search 모달창 선택 수정

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,31 +3,31 @@ import mapico from "../images/ico-map.svg";
 import calico from "../images/ico-calendar.svg";
 import siteico from "../images/ico-vector.svg";
 import Button from "./Button";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Modal from "./Modal";
 import DateModal from "./DateModal";
 
 // 지역 옵션 배열
 const locations = [
     "전체",
-    "서울특별시",
-    "충청남도",
-    "울산광역시",
-    "인천광역시",
+    "서울시",
+    "인천시",
     "경기도",
-    "대구광역시",
-    "강원특별자치도",
-    "부산광역시",
-    "대전광역시",
-    "세종특별자치시",
-    "광주광역시",
+    "강원도",
+    "대전시",
+    "세종시",
+    "대구시",
+    "부산시",
+    "울산시",
+    "전주시",
+    "광주시",
     "충청남도",
-    "전라남도",
     "충청북도",
-    "경상북도",
+    "전라남도",
+    "전라북도",
     "경상남도",
-    "전북특별자치도",
-    "제주특별자치도",
+    "경상북도",
+    "제주도",
 ];
 
 // 사이트 옵션 배열
@@ -37,6 +37,8 @@ const SearchBar = () => {
     const locationModal = useRef(null); // 위치 모달 관리
     const dateModal = useRef(null); // 날짜 및 일정 모달 관리
     const siteModal = useRef(null); // 캠프 사이트 모달 관리
+    const [activeLocation, setActiveLocation] = useState(0); // 위치 컨텐츠 버튼 상태 관리
+    const [activeSite, setActiveSite] = useState(0); // 캠프 사이트 버튼 상태 관리
 
     // 위치 모달 열기
     const openLocationModal = () => {
@@ -59,37 +61,41 @@ const SearchBar = () => {
         }
     };
 
+    // 검색바 버튼 옵션 배열
+    const searchBarButtons = [
+        {
+            label: "어디로 가세요?",
+            icon: <img src={mapico} width={"20px"} height={"20px"} />,
+            onClick: openLocationModal,
+        },
+        {
+            label: "날짜 및 일정",
+            icon: <img src={calico} width={"20px"} height={"20px"} />,
+            onClick: openDateModal,
+        },
+        {
+            label: "사이트 형태",
+            icon: <img src={siteico} width={"20px"} height={"20px"} />,
+            onClick: openSiteModal,
+        },
+    ];
+
     return (
         <>
             {/* 검색 바 */}
             <div className="search__bar">
-                <Button
-                    className={"btn-searchbar"}
-                    color={"secondary"}
-                    icon={<img src={mapico} width={"20px"} height={"20px"} />}
-                    iconPosition="left"
-                    onClick={openLocationModal}
-                >
-                    어디로 가세요?
-                </Button>
-                <Button
-                    className={"btn-searchbar"}
-                    color={"secondary"}
-                    icon={<img src={calico} width={"20px"} height={"20px"} />}
-                    iconPosition="left"
-                    onClick={openDateModal}
-                >
-                    날짜 및 일정
-                </Button>
-                <Button
-                    className={"btn-searchbar"}
-                    color={"secondary"}
-                    icon={<img src={siteico} width={"20px"} height={"20px"} />}
-                    iconPosition="left"
-                    onClick={openSiteModal}
-                >
-                    사이트 형태
-                </Button>
+                {searchBarButtons.map((button, index) => (
+                    <Button
+                        key={index}
+                        className="btn-searchbar"
+                        color="secondary"
+                        iconPosition="left"
+                        icon={button.icon}
+                        onClick={button.onClick}
+                    >
+                        {button.label}
+                    </Button>
+                ))}
                 <Button
                     color={"primary"}
                     icon={<img src={right_arr} />}
@@ -107,10 +113,14 @@ const SearchBar = () => {
                         <h2 className="location__title">어디로 가세요?</h2>
                     </div>
                     <div className="location__content">
-                        {locations.map((location) => (
+                        {locations.map((location, index) => (
                             <Button
-                                className={"btn-locationcontent"}
+                                key={index}
+                                className={`btn-content ${
+                                    activeLocation === index ? "active" : null
+                                }`}
                                 color={"secondary"}
+                                onClick={() => setActiveLocation(index)}
                             >
                                 <span className="content__name">
                                     {location}
@@ -129,10 +139,14 @@ const SearchBar = () => {
                 <div className="modal__site">
                     <h2 className="site__title">캠핑 사이트</h2>
                     <div className="site__container">
-                        {sites.map((site) => (
+                        {sites.map((site, index) => (
                             <Button
-                                className={"btn-sitecontent"}
+                                key={index}
+                                className={`btn-content ${
+                                    activeSite === index ? "active" : null
+                                }`}
                                 color={"secondary"}
+                                onClick={() => setActiveSite(index)}
                             >
                                 <span className="content__name">{site}</span>
                             </Button>
