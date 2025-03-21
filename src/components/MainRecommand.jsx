4a3dc22d8a084a580bd8e4ff7310efe7147131e3
DongWook <star00301@naver.com>
2025-03-21 19:29:02 +0900

    Feat. 메인 인트로부분 완성

--- src/components/MainRecommand.jsx
+++ src/components/MainRecommand.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
 import { fBService } from "../util/fbService";
+import { useQuery } from "@tanstack/react-query";
 
 const MainRecommand = () => {
-  const campsites = fBService.getSearchCampSite();
+  const { data } = useQuery({
+    queryKey: ["delay"],
+    queryFn: async () => fBService.getAllCampsites(),
+    select: (data) => {
+      return data[Math.floor(Math.random() * 30)].data;
+    },
+  });
 
-  campsites.then((data) => console.log("data", data));
+  console.log(data);
   return null;
 };
 
+// const campsite = data[Math.floor(Math.random() * 30)];
 export default MainRecommand;
