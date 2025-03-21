4e3d0a0a92fdcea1e1b8ea402d3a38d542330475
hyerimRoh <mtxpvksh@gmail.com>
2025-03-21 10:35:09 +0900

    Fix. 테스트용 버튼 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,8 +2,10 @@ import { useEffect } from "react";
 import TopBtn from "./../components/Topbtn";
 import ButtonExam from "./ButtonExam";
 import { fBService } from "../util/fbService";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     fBService.getAllReservation().then((result) => {
       console.log("%o", result);
@@ -15,6 +17,24 @@ const Main = () => {
   return (
     <div>
       <div>SAY HELLO</div>
+      <div>
+        <button
+          onClick={() => {
+            navigate("/detail");
+          }}
+        >
+          상세페이지 가기
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            navigate("/searchResult");
+          }}
+        >
+          검색결과 페이지 가기
+        </button>
+      </div>
       <TopBtn />
     </div>
   );
