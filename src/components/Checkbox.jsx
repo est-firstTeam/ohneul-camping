f3b5f6272b3d8f06790b5058f01af055d18accac
ShinJunYoung <andregray5929@gmail.com>
2025-03-21 13:46:12 +0900

    Chore. Checkbox 컴포넌트 HTML 구조 변경

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { useId } from "react";
 import checkIcon from "../images/ico-checked.svg";
 import unCheckedIcon from "../images/ico-uncheked.svg";
 
-const Checkbox = ({ checked, onClick, label }) => {
+const Checkbox = ({ checked, onChange, label }) => {
+  const id = useId();
   return (
-    <label className="checkbox-container">
-      {/* 실제 check 여부 확인 */}
-      <input type="checkbox" checked={checked} onChange={onClick} />
-      {/* 커스터마이징 checkbox */}
-      <span className="checkmark">
-        <img src={checked ? checkIcon : unCheckedIcon} alt="체크 아이콘" />
-      </span>
-      {label && <span className="label">{label}</span>}
-    </label>
+    <div className="checkbox-container">
+      {/* 실제 체크 여부 확인 */}
+      <input
+        type="checkbox"
+        id={id}
+        checked={checked}
+        onChange={onChange}
+        className="checkbox-input"
+      />
+
+      {/* 커스텀 체크박스 */}
+      <label htmlFor={id} className="checkbox-label">
+        <span className="checkmark">
+          <img src={checked ? checkIcon : unCheckedIcon} alt="체크 아이콘" />
+        </span>
+        {label && <span className="label">{label}</span>}
+      </label>
+    </div>
   );
 };
 
