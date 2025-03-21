e43bfe145b1186e46008b23714246e41c1f9b9c7
seung-yuppy <songsy0107@gmail.com>
2025-03-21 10:37:45 +0900

    Feat.search App라우팅 수정

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
           ],
         },
         {
-          path: "/searchResult",
+          path: "searchResult",
           element: <SearchResult />,
         },
         {
