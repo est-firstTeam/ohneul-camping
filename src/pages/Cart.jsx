e223471def42f6c547f3741968806507e9797970
hyerimRoh <mtxpvksh@gmail.com>
2025-03-25 14:48:45 +0900

    Feat. 이용약관 및 환불규정 모달 구현

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,19 +8,32 @@ import ProductListCart from "../components/ProductListCart";
 import { monthDateFormat } from "../util/util";
 import { useUserStore } from "../store/useUserStore";
 import { getDaysBetweenDates } from "../util/util";
+import { useRef } from "react";
+import Modal from "../components/Modal";
 
 const Cart = () => {
   const userId = useUserStore((state) => state.id);
   const { data: carts } = useQuery({
     queryKey: [`/cart/${userId}`],
     queryFn: () => fBService.getCartItems(userId),
+    // TODO: select: () => ???  파일분리
   });
-  // TODO: 해당하는 데이터 분리하기
+
   const { setTitle } = myPageTitleStore();
   useEffect(() => {
     setTitle("나의 장바구니");
   }, []);
 
+  const modalRef = useRef(null); // 위치 모달 관리
+  const openModal = (currentModal) => {
+    if (currentModal.current) {
+      currentModal.current.showModal();
+    }
+  };
+
+  // 환불규정 체크
+  const [isAgree, setIsAgree] = useState(false);
+  console.log(isAgree);
   // 체크박스 데이터
   const initialCheckedItems = {};
   const [checkedItems, setCheckedItems] = useState(initialCheckedItems);
@@ -111,9 +124,41 @@ const Cart = () => {
                 return cart.toString();
               }
             })}
+          <div className="cart__agreement">
+            <Checkbox id="agree" onChange={() => setIsAgree(!isAgree)} />
+            <button onClick={() => openModal(modalRef)}>
+              환불규정 및 약관 보기
+            </button>
+          </div>
         </section>
       </article>
-      {/* <Modal /> */}
+
+      {/* 이용약관 환불규정 구현 */}
+      <Modal modalRef={modalRef} completeText="동의합니다">
+        <div className="cart__modal">
+          <span className="cart__modal-title">이용 약관 및 환불규정</span>
+          <span className="cart__modal-content-title">내용</span>
+          <ul className="cart__modal-list">
+            <li>
+              본 사이트를 통해 예약한 캠핑장은 예약자 본인만 이용할 수 있습니다.
+            </li>
+            <li>
+              예약자는 캠핑장 이용 수칙을 준수해야 하며, 시설 훼손 시 배상
+              책임이 있습니다.
+            </li>
+            <li>현장 규정을 위반할 경우 이용이 제한될 수 있습니다.</li>
+          </ul>
+          <span className="cart__modal-content-title">환불 규정</span>
+          <ul className="cart__modal-list">
+            <li>이용일 2일 전까지 취소 시 100% 환불</li>
+            <li>이용일 1일 전부터는 환불 불가</li>
+            <li>
+              천재지변 등 불가피한 사유로 캠핑장이 운영되지 않을 경우 전액
+              환불됩니다.
+            </li>
+          </ul>
+        </div>
+      </Modal>
     </section>
   );
 };
