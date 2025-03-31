7375d4677ee836d39033510d38b01a474a418574
hyerimRoh <mtxpvksh@gmail.com>
2025-03-31 14:45:27 +0900

    Fix. 불필요한 코드 제거 - 애니메이션 시작위치 디자인 수정 - 유저데이터를 상태에서 가져오도록 수정

--- src/components/MainAllCampsite.jsx
+++ src/components/MainAllCampsite.jsx
@@ -44,33 +44,31 @@ export default function MainAllCampsite() {
     <section className="all-campsite">
       <h2>오늘 어디 갈래?</h2>
       <h3>모든 캠핑장 정보</h3>
-      <div ref={reservation}>
-        <ul className="all-campsite__wrapper">
-          {items.map((campObj) => {
-            return (
-              <motion.li
-                className="all-campsite__item"
-                key={campObj.id}
-                initial={{ opacity: 0, y: 0 }}
-                animate={{ opacity: 1, y: -30 }}
-                transition={{ type: "tween", delay: 0.1 }}
-                whileHover={{
-                  scale: 1.05,
-                  cursor: "pointer",
-                  transition: {
-                    duration: 0.2,
-                  },
-                }}
-                onClick={() => {
-                  navi(`/searchResult/${campObj.id}`);
-                }}
-              >
-                <ProductMain camp={campObj} />
-              </motion.li>
-            );
-          })}
-        </ul>
-      </div>
+      <ul className="all-campsite__wrapper" ref={reservation}>
+        {items.map((campObj) => {
+          return (
+            <motion.li
+              className="all-campsite__item"
+              key={campObj.id}
+              initial={{ opacity: 0, y: +30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ type: "tween", delay: 0.1 }}
+              whileHover={{
+                scale: 1.05,
+                cursor: "pointer",
+                transition: {
+                  duration: 0.2,
+                },
+              }}
+              onClick={() => {
+                navi(`/searchResult/${campObj.id}`);
+              }}
+            >
+              <ProductMain camp={campObj} />
+            </motion.li>
+          );
+        })}
+      </ul>
     </section>
   );
 }
