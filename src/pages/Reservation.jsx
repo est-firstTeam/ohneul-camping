6451373c32d90895f62577e27cf6c69f73846dbf
coding-pop <dsnpb11@gmail.com>
2025-04-01 10:06:25 +0900

    Feat/Reservation 컴포넌트 배열 수정

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -183,7 +183,7 @@ const Reservation = () => {
   return (
     <section className="reservation">
       <h2 className="reservation__title ">예약 확인 페이지 입니다.</h2>
-      <div className="product-list">
+      <div className="reservation__list">
         {reservationData?.length > 0 ? (
           reservationData.map((reservation) => {
             const isCanceled = reservation.data?.rsvIsCanceled === true;
@@ -245,8 +245,10 @@ const Reservation = () => {
           <div className="modal__rsvcancel">
             {modalStep === "confirm" ? (
               <>
-                <h2 className="modal__title">예약을 취소하시겠습니까?</h2>
-                <div className="modal__content">
+                <h2 className="modal__rsvcancel-title">
+                  예약을 취소하시겠습니까?
+                </h2>
+                <div className="modal__rsvcancel-content">
                   예약을 취소하시면 되돌릴 수 없습니다.
                   <br />
                   예약 취소를 원하실 경우 '확인' 버튼을 클릭해 주세요.
@@ -254,8 +256,10 @@ const Reservation = () => {
               </>
             ) : (
               <>
-                <h2 className="modal__title">예약이 취소되었습니다.</h2>
-                <div className="modal__content">
+                <h2 className="modal__rsvcancel-title">
+                  예약이 취소되었습니다.
+                </h2>
+                <div className="modal__rsvcancel-content">
                   예약 현황 목록에서 확인하실 수 있습니다.
                 </div>
               </>
