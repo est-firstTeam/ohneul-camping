8ce47097a1686033281ccd2b47eeea6c0d78fac8
DongWook <star00301@naver.com>
2025-04-03 14:14:39 +0900

    Feat. 정보변경 시 사용자 재인증과정 추가

--- src/constants/errorCodes.js
+++ src/constants/errorCodes.js
@@ -1,7 +1,8 @@
 export const errorCodes = {
   "auth/email-already-in-use": "이메일이 이미 사용중입니다.",
-  "auth/invalid-credential": "이메일과 비밀번호를 확인해주세요.",
+  "auth/invalid-credential": "입력한 정보를 다시 확인해주세요.",
   "auth/missing-email": "올바른 이메일 주소를 입력해주세요.",
   "auth/invalid-email": "이메일 형식을 확인해주세요.",
   "auth/unauthorized-domain": "가입된 회원이 없습니다.",
+  "auth/too-many-requests": "너무 많은 요청. 잠시 후 다시 시도해주세요.",
 };
